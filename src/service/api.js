70a57de76250edfb99faa0ffb600043ee261d908
Rishi <rishimishra18052001@gmail.com>
2023-10-03 17:33:17 +0700

    new change

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,6 @@
 import axios from 'axios';
-
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
-import { getAccessToken, getRefreshToken, setAccessToken, getType } from '../utils/common-utils';
+import { getAccessToken, setAccessToken, getType } from '../utils/common-utils';
 
 const API_URL = 'https://copper-swallow-fez.cyclic.cloud';
 
@@ -9,14 +8,14 @@ const axiosInstance = axios.create({
     baseURL: API_URL,
     timeout: 10000,
     headers: {
-        "content-type": "application/json"
+        "Content-Type": "application/json"
     }
 });
 
 axiosInstance.interceptors.request.use(
     function (config) {
         if (config.TYPE.params) {
-            config.params = config.TYPE.params
+            config.params = config.TYPE.params;
         } else if (config.TYPE.query) {
             config.url = config.url + '/' + config.TYPE.query;
         }
@@ -29,87 +28,41 @@ axiosInstance.interceptors.request.use(
 
 axiosInstance.interceptors.response.use(
     function (response) {
-        // Stop global loader here
         return processResponse(response);
     },
     function (error) {
-        // Stop global loader here
-        return Promise.reject(ProcessError(error));
+        return Promise.reject(processError(error));
     }
-)
+);
 
-///////////////////////////////
-// If success -> returns { isSuccess: true, data: object }
-// If fail -> returns { isFailure: true, status: string, msg: string, code: int }
-//////////////////////////////
-const processResponse = (response) => {
-    if (response?.status === 200) {
-        return { isSuccess: true, data: response.data }
-    } else {
-        return {
-            isFailure: true,
-            status: response?.status,
-            msg: response?.msg,
-            code: response?.code
-        }
-    }
-}
-
-///////////////////////////////
-// If success -> returns { isSuccess: true, data: object }
-// If fail -> returns { isError: true, status: string, msg: string, code: int }
-//////////////////////////////
-const ProcessError = async (error) => {
+const processError = async (error) => {
     if (error.response) {
-        // Request made and server responded with a status code 
-        // that falls out of the range of 2xx
         if (error.response?.status === 403) {
-            // const { url, config } = error.response;
-            // console.log(error);
-            // try {
-            //     let response = await API.getRefreshToken({ token: getRefreshToken() });
-            //     if (response.isSuccess) {
             sessionStorage.clear();
-            //         setAccessToken(response.data.accessToken);
-
-            //         const requestData = error.toJSON();
-
-            //         let response1 = await axios({
-            //             method: requestData.config.method,
-            //             url: requestData.config.baseURL + requestData.config.url,
-            //             headers: { "content-type": "application/json", "authorization": getAccessToken() },
-            //             params: requestData.config.params
-            //         });
-            //     }
-            // } catch (error) {
-            //     return Promise.reject(error)
-            // }
         } else {
             console.log("ERROR IN RESPONSE: ", error.toJSON());
             return {
                 isError: true,
                 msg: API_NOTIFICATION_MESSAGES.responseFailure,
                 code: error.response.status
-            }
+            };
         }
     } else if (error.request) {
-        // The request was made but no response was received
         console.log("ERROR IN RESPONSE: ", error.toJSON());
         return {
             isError: true,
             msg: API_NOTIFICATION_MESSAGES.requestFailure,
             code: ""
-        }
+        };
     } else {
-        // Something happened in setting up the request that triggered an Error
         console.log("ERROR IN RESPONSE: ", error.toJSON());
         return {
             isError: true,
             msg: API_NOTIFICATION_MESSAGES.networkError,
             code: ""
-        }
+        };
     }
-}
+};
 
 const API = {};
 
@@ -121,7 +74,7 @@ for (const [key, value] of Object.entries(SERVICE_URLS)) {
             data: value.method === 'DELETE' ? '' : body,
             responseType: value.responseType,
             headers: {
-                authorization: getAccessToken(),
+                Authorization: getAccessToken(),
             },
             TYPE: getType(value, body),
             onUploadProgress: function (progressEvent) {
