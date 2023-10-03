4274c42bc29b753a3fd9051b2ac99a86866867db
Rishi <rishimishra18052001@gmail.com>
2023-10-03 17:20:58 +0700

    backend issue

--- src/service/api.js
+++ src/service/api.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 import { API_NOTIFICATION_MESSAGES, SERVICE_URLS } from '../constants/config';
 import { getAccessToken, getRefreshToken, setAccessToken, getType } from '../utils/common-utils';
 
-const API_URL = 'https://blogit-backend-s19u.onrender.com';
+const API_URL = 'https://copper-swallow-fez.cyclic.cloud';
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
